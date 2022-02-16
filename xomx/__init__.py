2219023646f4e7b4a90668323639eb73eda6dddd
Nicolas Perrin <perrin.isir@gmail.com>
2022-02-16 17:29:42 +0100

    adding supervised contrastive learnign

--- xomx/__init__.py
+++ xomx/__init__.py
@@ -1,6 +1,7 @@
 from . import tools as tl
 from . import data_importation as di
 from . import classifiers as cl
+from . import embeddings as em
 from . import feature_selection as fs
 from . import plotting as pl
 from . import tutorials as tt
