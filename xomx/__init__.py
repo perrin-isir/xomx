958ff68c2eb827a3191a1e113d2f0f1c3758c60d
Nicolas Perrin <perrin.isir@gmail.com>
2022-02-27 18:04:22 +0100

    checking import speed

--- xomx/__init__.py
+++ xomx/__init__.py
@@ -1,6 +1,12 @@
 from . import tools as tl
+print("imported")
 from . import data_importation as di
+print("imported")
 from . import classifiers as cl
+print("imported")
 from . import embeddings as em
+print("imported")
 from . import feature_selection as fs
+print("imported")
 from . import plotting as pl
+print("imported")
--- xomx/plotting/logos.py
+++ xomx/plotting/logos.py
@@ -2,7 +2,7 @@ import numpy as np
 from pandas import DataFrame
 from scanpy import AnnData
 from xomx.tools.bio import aminoacids
-import logomaker
+from logomaker import Logo
 import matplotlib.pyplot as plt
 from scipy.stats import entropy
 
@@ -60,11 +60,11 @@ def plot_logo(adata: AnnData, indices, fixed_length: int = None):
     """
     df = compute_logo_df(adata, indices, fixed_length)
     fig, ax = plt.subplots(1, 1, figsize=[4, 2])
-    logo = logomaker.Logo(df,
-                          ax=ax,
-                          baseline_width=0,
-                          show_spines=False,
-                          vsep=.005,
-                          width=.95)
+    logo = Logo(df,
+                ax=ax,
+                baseline_width=0,
+                show_spines=False,
+                vsep=.005,
+                width=.95)
     logo.fig.tight_layout()
     plt.show()
