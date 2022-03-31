f4e8e962525c1ce37c5d9043b0f09fef3a8fb466
Nicolas Perrin <perrin.isir@gmail.com>
2022-03-31 11:44:13 +0200

    custom modif to nipy_spectral

--- xomx/plotting/__init__.py
+++ xomx/plotting/__init__.py
@@ -7,3 +7,12 @@ from .basic_plot import (
     plot_2d_obsm,
     plot_2d_embedding,
 )
+
+import matplotlib.pyplot as plt
+
+# custom modification to the nipy_spectral colormap
+nipy_spectral_colormap = plt.get_cmap("nipy_spectral")
+nipy_spectral_colormap._segmentdata["red"][-1] = (1.0, 1.0, 1.0)
+nipy_spectral_colormap._segmentdata["green"][-1] = (1.0, 0.0, 0.0)
+nipy_spectral_colormap._segmentdata["blue"][-1] = (1.0, 0.0, 0.0)
+nipy_spectral_colormap._init()
