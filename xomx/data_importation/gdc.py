1534fda9d86b405ccb39b56764c610bdf0a39b87
Nicolas Perrin <perrin.isir@gmail.com>
2022-04-04 19:12:55 +0200

    Mathias's modifs to gdc imports

--- xomx/data_importation/gdc.py
+++ xomx/data_importation/gdc.py
@@ -49,12 +49,12 @@ def gdc_create_manifest(disease_type, project_list, nr_of_cases_list):
                     "op": "in",
                     "content": {
                         "field": "files.analysis.workflow_type",
-                        "value": ["HTSeq - Counts"],
+                        "value": ["STAR - Counts"],
                     },
                 },
                 {
                     "op": "in",
-                    "content": {"field": "files.data_format", "value": ["TXT"]},
+                    "content": {"field": "files.data_format", "value": ["TSV"]},
                 },
             ],
         }
@@ -76,6 +76,7 @@ def gdc_create_manifest(disease_type, project_list, nr_of_cases_list):
                 "cases.0.project.project_id": "annotation",
             }
         )
+        df = df[~df["filename"].str.endswith("tsv.gz")]
         df = df[["id", "filename", "md5", "size", "state", "annotation"]]
         df_list.append(df)
     return df_list
@@ -95,8 +96,10 @@ def gdc_create_data_matrix(dir_path, manifest_path):
                 read_table(
                     os.path.join(dir_path, manifest["id"][i], manifest["filename"][i]),
                     header=None,
-                )
-                .rename(columns={1: manifest["id"][i]})
+                    delimiter="\t",
+                    skiprows=6,
+                )[[0, 3]]
+                .rename(columns={3: manifest["id"][i]})
                 .set_index(0)
             )
 
