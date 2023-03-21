f17dcce1851798b9e6515b5c858c122a9fe6b2d1
Nicolas Perrin <perrin.isir@gmail.com>
2023-03-21 16:37:32 +0100

    added release.yml

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup, find_packages
-
 # Install with 'pip install -e .'
+
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="xomx",
     version="0.1.0",
     author="Nicolas Perrin-Gilbert",
     description="xomx: a python library for computational omics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/perrin-isir/xomx",
     packages=find_packages(),
     install_requires=[
